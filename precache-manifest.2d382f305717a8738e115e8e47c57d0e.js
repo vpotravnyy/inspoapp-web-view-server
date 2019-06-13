9a7aa58c16e1363288f75bb5c1a4a4877608cb46
Slava Potravnyy <slava.potravnyy@headbox.com>
2019-06-13 14:17:13 +0300

    1st

---
+++ precache-manifest.2d382f305717a8738e115e8e47c57d0e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad68571f8f2a0aeb58a32aa29e891c1",
+    "url": "/inspoapp-web-view-server/index.html"
+  },
+  {
+    "revision": "60e11fc0011adbb71551",
+    "url": "/inspoapp-web-view-server/static/css/main.e8ffb73f.chunk.css"
+  },
+  {
+    "revision": "f4e22f688c6e5f79c746",
+    "url": "/inspoapp-web-view-server/static/js/2.718f083f.chunk.js"
+  },
+  {
+    "revision": "60e11fc0011adbb71551",
+    "url": "/inspoapp-web-view-server/static/js/main.0a94ff12.chunk.js"
+  },
+  {
+    "revision": "c0ec4a3d49f779034ec1",
+    "url": "/inspoapp-web-view-server/static/js/runtime~main.f4f706b6.js"
+  },
+  {
+    "revision": "ce792ace742f8cf0c8c48b6eecc16a32",
+    "url": "/inspoapp-web-view-server/static/media/Montserrat-Medium.ce792ace.woff2"
+  },
+  {
+    "revision": "e7a7c8f647da92cf34719e0203cc0aed",
+    "url": "/inspoapp-web-view-server/static/media/SackersGothicStd-Medium.e7a7c8f6.woff2"
+  },
+  {
+    "revision": "0289a65fbd4c7c1ce164b8acb0c11473",
+    "url": "/inspoapp-web-view-server/static/media/icon.0289a65f.svg"
+  },
+  {
+    "revision": "590b69af942c5ccdfb4efb0710865116",
+    "url": "/inspoapp-web-view-server/static/media/icon.590b69af.svg"
+  }
+]);
